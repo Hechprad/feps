8b1ff36a7340b6a33651256ccf5b45db7d2ee42f
Jorge Hecherat <hecherat@gmail.com>
2019-08-31 10:10:32 -0300

    arquivos iniciais e rotas do site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**mapeando e trocando as rotas */
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+/**importando as páginas */
+import Main from './Pages/Main';
+import BookView from './Pages/BookView';
+import BookNew from './Pages/BookView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' component={Main} />
+        <Route path='/book/new' component={BookNew} />
+        <Route path='/book/:id' component={BookView} />
+      </Switch>
+    </Router>
   );
 }
 
