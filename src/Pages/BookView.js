e16f17ab06643aaadadf99d4152f232332d5abb6
Jorge Hecherat <hecherat@gmail.com>
2019-08-31 23:32:01 -0300

    boa noite

--- src/Pages/BookView.js
+++ src/Pages/BookView.js
@@ -1,7 +1,40 @@
-// import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const BookView = () => {
+  
 
 };
 
 export default BookView;
+
+// import React, { useState, useEffect } from 'react'
+// import Header from '../Components/Header'
+// import Hero from '../Components/Header/Hero'
+// import MovieDetail from '../Components/MovieDetail'
+// import MovieForm from '../Components/MovieForm'
+// import { movies } from '../mock'
+
+// const MovieView = ({ match }) => {
+//   const [movie, setMovie] = useState(null);
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   const handleEdit = () => {
+//     setIsEditing(!isEditing)
+//   }
+//   useEffect(() => {
+//     const filtered = movies.filter((movie) => movie.id === parseInt(match.params.id))[0]
+//     setMovie(filtered)
+//   }, [match.params.id]);
+
+//   return (
+//     <div>
+//       <Header />
+//       <Hero />
+//       {
+//         isEditing ?
+//           <MovieForm movie={movie} /> :
+//           <MovieDetail movie={movie} handleEdit={handleEdit} />
+//       }
+//     </div>
+//   )
+// }
